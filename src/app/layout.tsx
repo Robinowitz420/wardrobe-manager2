7c042cd6ba7ae865fe80b719fc82db7448f17ea8
Maria Castro <mariacastro@MariaCastro-Codecademy-MacBookPro.lan>
2026-02-28 00:58:23 -0500

    Fix wallpaper: remove conflicting Tailwind classes, use CSS !important

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[url('/wallpaper.png')] bg-cover bg-center bg-fixed`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
           attribute="class"
